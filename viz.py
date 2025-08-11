c7a223aab01049902083830441d0e52b43a60704
e.kondrashov <egorkondr4@gmail.com>
2025-08-11 17:14:35 +0300

    files refactoring

--- viz.py
+++ viz.py
@@ -1,15 +1,7 @@
-from pm4py.objects.log.importer.xes import importer as xes_importer
-from pm4py.visualization.dfg import visualizer as dfg_visualizer
-from pm4py.objects.conversion.log import converter as log_converter
-from pm4py.algo.discovery.dfg import algorithm as dfg_factory
+import pm4py
 
+if __name__ == "__main__":
+    log = pm4py.read_xes('lstm_log.xes')
 
-log_path = "lstm_log.xes"
-log = xes_importer.apply(log_path)
-
-event_log = log_converter.apply(log, variant=log_converter.Variants.TO_EVENT_LOG)
-
-dfg = dfg_factory.apply(event_log)
-
-dfg_gviz = dfg_visualizer.apply(dfg, log=event_log, variant=dfg_visualizer.Variants.FREQUENCY)
-dfg_visualizer.view(dfg_gviz)
+    dfg, start_activities, end_activities = pm4py.discover_dfg(log)
+    pm4py.view_dfg(dfg, start_activities, end_activities)
